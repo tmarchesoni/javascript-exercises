be25f697a09e877ba649095a32452f81d98ab660
Gustavo Thomaz <136711692+tmarchesoni@users.noreply.github.com>
2026-07-07 16:25:55 -0300

    Modify greeting to include age based on birth year
    Update greeting string to include dynamic age calculation.

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -4,14 +4,7 @@
  This year: 1965
  Birth year: 1947
 
- The greeting should read: "Hello! My name is Carlos Stevenson and I am 18 years old."
-*/
-
-// Add your code right below, good luck!
-
-
-
-
+const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
 
 // Do not change this
 module.exports = {
